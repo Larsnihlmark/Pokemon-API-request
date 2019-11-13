f5d754790b1d7f488d22bc5fff45a81841e39332
Lars Nihlmark <larsnihlmark@gmail.com>
2019-11-13 18:48:36 +0100

    Added style

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require("express")
 const app = express()
 const port = 9999
 
-app.get("/", express.static("public"));
+
+app.use('/', express.static('public'))
 
 app.use(function (req,res,next){
-    res.status(404).send("Sorry cant find it!");
-  });
 
-app.listen(port, () => console.log(`Up and running on localhost: ${port}`))
+    res.status(404).send("Sorry cant find it! :O");
+  })
+
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
+
+app.listen(port, () => console.log(`Up and running on localhost: ${port}!`))
